67c10323c7b1750e996d17afbe948c1b5958c987
briansomogyi <brian.somogyi@emanuel.ro>
2025-01-30 23:49:22 +0200

    Created the "Navbar" Component

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <nav className="navbar navbar-light bg-light">
+            <a className="navbar-brand" href="#">
+                <i className="bi bi-instagram"></i> Instagram Clone
+            </a>
+        </nav>
+    );
+};
+
+export default Navbar;
